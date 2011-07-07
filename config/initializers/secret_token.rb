8ce18776a7dda9a92b63fdd82382193f12a2603c
Will Franklin <willfrnkln@gmail.com>
2011-07-07 13:06:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afghanistanitors::Application.config.secret_token = 'fd1b5898d55997ae7cb1627a87e6eedbd6948e40c015e6b41d79d4a581976b47d91dc2c867ca2606307bf5f4c0478700ecd17433bd8e3b6db49344693a7d7cf7'
